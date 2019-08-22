65bafdae0edb8885143ee968cb7eb0ad0d741f13
bai.jingfeng <bai.jingfeng@huadongmedia.com>
2019-08-22 18:34:30 +0800

    update: caceh

--- command/add.js
+++ command/add.js
@@ -4,3 +4,36 @@ const prompt = require('co-prompt')
 const chalk = require('chalk')
 const fs = require('fs')
 const config = require('../templates')
+
+module.exports = () => {
+  co(function*() {
+    // 分步接收用户输入的参数
+    let tplName = yield prompt('Template name: ')
+    let gitUrl = yield prompt('Git https link: ')
+    let branch = yield prompt('Branch: ')
+
+    // 避免重复添加
+    if (!config.tpl[tplName]) {
+      config.tpl[tplName] = {}
+      // 过滤Unicode字符
+      config.tpl[tplName]['url'] = gitUrl.replace(/[\u0000-\u0019]/g, '')
+      config.tpl[tplName]['branch'] = branch
+    } else {
+      console.log(chalk.red('Template has already existed!'))
+      process.exit()
+    }
+
+    // 将模板信息写入templates.json
+    fs.writeFile(__dirname + '/../templates.json', JSON.stringify(config), 'utf-8', (err) => {
+      if (err) {
+        console.log(err)
+      }
+
+      console.log(chalk.green('New template added!\n'))
+      console.log(chalk.grey('The last template list is: \n'))
+      console.log(config)
+      console.log('\n')
+      process.exit()
+    })
+  })
+}
